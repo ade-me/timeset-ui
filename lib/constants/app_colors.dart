a57dbb76434c9bb78df3fad7b76d8706ad1b456e
Ariyo <osuolaleariyo@gmail.com>
2024-05-14 12:24:12 +0100

    Added app colors class

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class AppColors {
+  static Color primary = HexColor("#9CBB30");
+  static Color scaffoldColor = HexColor("#141414");
+  static Color selectionColor = HexColor("#2C2C2C");
+}
