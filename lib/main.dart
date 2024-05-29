3e92bf05159695f3f5e072b630701ce946035168
Ariyo <osuolaleariyo@gmail.com>
2024-05-29 06:27:06 +0100

    Init chat

--- lib/main.dart
+++ lib/main.dart
@@ -99,7 +99,7 @@ class MyApp extends StatelessWidget {
                 selectionHandleColor: AppColors.primary,
               ),
             ),
-            home: Provider.of<AuthProvider>(context).isLoggedIn
+            home: !Provider.of<AuthProvider>(context).isLoggedIn
                 ? const HomeScreen()
                 : const Onboarding(),
             onGenerateRoute: (settings) =>
